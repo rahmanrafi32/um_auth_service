3031b4aa0e3fe759d89fdadd6fa467e20a34bded
Md Minhazur Rahman <rafi.rahman03@yahoo.com>
2023-05-31 00:16:22 +0600

    [Done] setup express server.

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, {Express, Request, Response} from 'express';
+import cors from 'cors';
+
+export const app: Express = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json("server is running...")
+});
+
