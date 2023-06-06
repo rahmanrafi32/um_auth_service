b76d30d78b26069652a6c8a0e51a4b68d4c4a983
Minhazur Rahman <rafi.rahman03@yahoo.com>
2023-06-06 20:42:41 +0600

    [Done] Setting up error handling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import userRoute from './modules/users/user.route'
+import globalErrorHandler from './middlewares/globalErrorHandler'
+
 export const app: Express = express()
 
 app.use(cors())
@@ -8,6 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/users', userRoute)
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response): void => {
   res.status(200).json({ message: 'server is running successfully' })
 })
+
+app.use(globalErrorHandler)
--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
   dbUrl: process.env.DB_URL,
 }
