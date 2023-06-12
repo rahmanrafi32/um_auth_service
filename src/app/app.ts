4a63115aa1c17930ace1bb88251aff49d5b12d21
Minhazur Rahman <rafi.rahman03@yahoo.com>
2023-06-12 23:04:02 +0600

    refactored app.ts file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express } from 'express'
 import cors from 'cors'
-import userRoute from './modules/users/user.route'
 import globalErrorHandler from './middlewares/globalErrorHandler'
+import routes from './routes'
 
 export const app: Express = express()
 
@@ -9,9 +9,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/v1/users', userRoute)
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'server is running successfully' })
-})
+app.use('/api/v1/', routes)
 
 app.use(globalErrorHandler)
