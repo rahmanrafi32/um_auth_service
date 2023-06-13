022016971958425fc37054b35f73462a02aa39c6
Minhazur Rahman <rafi.rahman03@yahoo.com>
2023-06-13 12:33:25 +0600

    not found error handler added.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import express, { Express } from 'express'
+import express, { Express, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 import routes from './routes'
+import httpStatus from 'http-status'
 
 export const app: Express = express()
 
@@ -11,4 +12,20 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/', routes)
 
+//global handler
 app.use(globalErrorHandler)
+
+//not found error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  })
+  next()
+})
